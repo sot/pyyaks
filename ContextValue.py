42f54478af29944d26e7ecfb2ce9076d95a134ec
aldcroft <devnull@localhost>
2008-09-20 02:57:20 +0000

    - Substantial reorganization.

--- ContextValue.py
+++ ContextValue.py
@@ -13,8 +13,7 @@ except RuntimeError, msg:
 
 def render(s):
     """Convenience function to create an anonymous ContextValue and then render it."""
-    x = ContextValue(s)
-    return str(x)
+    return str(Value(s))
 
 def render_func(func):
     """Wrap func so that its first arg is rendered"""
@@ -42,7 +41,7 @@ def _relative_path(path, basedir=None, currdir=None):
 
     """
     if currdir is None:
-        currdir = os.curdir
+        currdir = os.getcwd()
     if basedir is None:
         basedir = currdir
 
@@ -66,68 +65,30 @@ def _relative_path(path, basedir=None, currdir=None):
     relpaths = [os.pardir] * len(currpaths) + destpaths
     return os.path.join(*relpaths)
 
-class ContextDict(dict):
-    """Dictionary class that automatically registers the dict in the Context and
-    overrides __setitem__ to create an appropriate ContextValue when assigning
-    to a dict key.  Example:
-
-    src = ContextDict('src')  # Make Context dict with root name 'src'
-    src['obsid'] = 123
-    files = ContextDict('file') # Context dict with root name 'file'
-    files['src']  = 'data/obs{{ src.obsid }}'
-    files['evt2'] = '{{ file.src }}/acis_evt2.fits'
-    """
-    def __init__(self, name, **kwargs):
-        dict.__init__(self)
-        Context[name] = self
-        self.name = name
-        self.kwargs = kwargs 
-
-    def __setitem__(self, key, val):
-        if isinstance(val, ContextValue):
-            dict.__setitem__(self, key, val)
-        else:
-            # If ContextValue was already init'd then just update val
-            if key in self:
-                dict.__getitem__(self, key).val = val
-            else:
-                dict.__setitem__(self, key, ContextValue(val, key, **self.kwargs))
-
-class ContextValue(object):
+class Value(object):
     def getval(self): return self.__val
-    def setval(self, x):
-        self.__val = x
+
+    def setval(self, val):
+        self.__val = val
         try:
-            self.template = django.template.Template(x + '')
+            self.template = django.template.Template(val + '')
         except TypeError:
             self.template = None
+
     val = property(getval, setval)
 
-    def __init__(self, val=None, name=None, format=None, basedir=None):
-        if isinstance(val, ContextValue):
+    def __init__(self, val=None, name=None):
+        if isinstance(val, self.__class__):
             self = val
-            return
-        
-        self.format = format
-        self.name = name
-        if name:
-            # Add to Context.  Some error checking here would be good
-            # if e.g. name=var.subvar and var exists already but is not dict
-            names = name.split('.')
-            vc = Context
-            for name in names[:-1]:
-                if name not in vc:
-                    vc[name] = {}
-                vc = vc[name]
-            vc[names[-1]] = self
-        self.val = val
-        self.basedir = basedir and os.path.abspath(basedir) or None
+        else:
+            self.val = val
+            self.name = name
 
     def __unicode__(self):
         return str(self)
-
+    
     def __str__(self):
-        if self.template:
+        if self.template is not None:
             django_context = django.template.Context(Context)
             val = self.template.render(django_context)
             while (re.search(r'{[%{]', val)):
@@ -137,18 +98,16 @@ class ContextValue(object):
                 else:
                     val = newval
         else:
-            val = self.val
-                
-        if self.format:
-            strval = self.format % val
-        else:
-            strval = str(val)
+            val = str(self.val)
+        return val
 
-        # If basedir is defined then this is a file so render as a relative path
-        if self.basedir:
-            strval = _relative_path(strval, basedir=self.basedir)
+class File(Value):
+    def __init__(self, val=None, name=None, basedir=None):
+        super(File, self).__init__(val, name)
+        self.basedir = basedir and os.path.abspath(basedir) or os.getcwd()
 
-        return strval
+    def __str__(self):
+        return _relative_path(super(File, self).__str__(), self.basedir)
 
     def getrel(self):
         return str(self)
@@ -163,4 +122,33 @@ class ContextValue(object):
 
     rel = property(getrel)
     abs = property(getabs)
+
+class Dict(dict):
+    """Dictionary class that automatically registers the dict in the Context and
+    overrides __setitem__ to create an appropriate ContextValue when assigning
+    to a dict key.  Example:
+
+    src = ContextDict('src')  # Make Context dict with root name 'src'
+    src['obsid'] = 123
+    files = ContextDict('file') # Context dict with root name 'file'
+    files['src']  = 'data/obs{{ src.obsid }}'
+    files['evt2'] = '{{ file.src }}/acis_evt2.fits'
+    """
+    def __init__(self, name, valuetype=Value, **kwargs):
+        dict.__init__(self)
+        Context[name] = self
+        self.name = name
+        self.valuetype = valuetype
+        self.kwargs = kwargs
+        
+
+    def __setitem__(self, key, val):
+        if isinstance(val, self.valuetype):
+            dict.__setitem__(self, key, val)
+        else:
+            # If ContextValue was already init'd then just update val
+            if key in self:
+                dict.__getitem__(self, key).val = val
+            else:
+                dict.__setitem__(self, key, self.valuetype(val, key, **self.kwargs))
 
