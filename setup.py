0370be09269b3a9db23f7ee49922ec8c491d3c55
Tom Aldcroft <aldcroft@head.cfa.harvard.edu>
2009-09-11 14:51:14 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='cosmocalc',
+      # url='http://cxc.harvard.edu/contrib/cosmocalc',
+      version='0.1',
+      description='Pipeline processing toolkit',
+      author='Tom Aldcroft',
+      author_email='aldcroft@head.cfa.harvard.edu',
+      packages=['pyyaks'],
+      )
