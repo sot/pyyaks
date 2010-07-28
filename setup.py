4a64401dae9a9e6401712542ad3527bc979734d5
Tom Aldcroft <aldcroft@head.cfa.harvard.edu>
2010-07-28 12:13:38 -0400

    Update setup.py: new version and url

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='pyyaks',
-      # url='http://cxc.harvard.edu/contrib/cosmocalc',
-      version='0.1',
+      url='http://cxc.harvard.edu/contrib/pyyaks',
+      version='0.2',
       description='Pipeline processing toolkit',
       author='Tom Aldcroft',
       author_email='aldcroft@head.cfa.harvard.edu',
