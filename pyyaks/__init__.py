7c9af157ec92ae61941daff35aebc5f50b10a07a
Tom Aldcroft <taldcroft@gmail.com>
2016-12-26 14:43:52 -0500

    Modernize tests

--- pyyaks/__init__.py
+++ pyyaks/__init__.py
@@ -1,13 +1,11 @@
 from __future__ import print_function
 
-def test(*args, **kwargs):
-    import os
-    import pytest
-    pkg_path = os.path.dirname(os.path.abspath(__file__))
-    pkg_rootdir, pkg_name = os.path.split(pkg_path)
-    os.chdir(pkg_rootdir)
-    args = (pkg_name,) + args
-    print(pkg_rootdir, pkg_name)
-    pytest.main(list(args), **kwargs)
 
-__version__ = '3.3.3'
+def test(*args, **kwargs):
+    '''
+    Run py.test unit tests.
+    '''
+    import testr
+    return testr.test(*args, **kwargs)
+
+__version__ = '3.3.4'
