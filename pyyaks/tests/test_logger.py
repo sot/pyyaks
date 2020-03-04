5096b96e99751d9f711255db0ee3b8711c94b02b
Tom Aldcroft <taldcroft@gmail.com>
2020-03-04 09:37:24 -0500

    Fix tests to pass on Windows

--- pyyaks/tests/test_logger.py
+++ pyyaks/tests/test_logger.py
@@ -2,6 +2,7 @@
 from __future__ import print_function, division, absolute_import
 
 import tempfile
+import os
 from .. import logger as pyyaks_logger
 import six
 
@@ -35,29 +36,29 @@ def test_stream():
     logger.info('Info')
     logger.warning('Warning')
     assert stdout.getvalue() == "Info\nWarning\n"
-    
-def test_file():
-    tmp = tempfile.NamedTemporaryFile()
-    logger = pyyaks_logger.get_logger(filename=tmp.name, stream=None)
+
+def test_file(tmpdir):
+    tmp = os.path.join(tmpdir, 'tmp.log')
+    logger = pyyaks_logger.get_logger(filename=tmp, stream=None)
     logger.debug('Debug')
     logger.info('Info')
     logger.warning('Warning')
-    assert open(tmp.name).read() == "Info\nWarning\n"
-    
-def test_redefine():
+    assert open(tmp).read() == "Info\nWarning\n"
+
+def test_redefine(tmpdir):
     stdout1 = six.StringIO()
     stdout2 = six.StringIO()
-    tmp1 = tempfile.NamedTemporaryFile()
-    tmp2 = tempfile.NamedTemporaryFile()
-    logger = pyyaks_logger.get_logger(filename=tmp1.name, stream=stdout1, filelevel=pyyaks_logger.WARNING)
+    tmp1 = os.path.join(tmpdir, 'tmp1.log')
+    tmp2 = os.path.join(tmpdir, 'tmp2.log')
+    logger = pyyaks_logger.get_logger(filename=tmp1, stream=stdout1, filelevel=pyyaks_logger.WARNING)
     logger.debug('Debug1')
     logger.info('Info1')
     logger.warning('Warning1')
-    logger = pyyaks_logger.get_logger(filename=tmp2.name, stream=stdout2, level=pyyaks_logger.DEBUG)
+    logger = pyyaks_logger.get_logger(filename=tmp2, stream=stdout2, level=pyyaks_logger.DEBUG)
     logger.debug('Debug2')
     logger.info('Info2')
     logger.warning('Warning2')
-    assert open(tmp1.name).read() == "Warning1\n"
+    assert open(tmp1).read() == "Warning1\n"
     assert stdout1.getvalue() == "Info1\nWarning1\n"
-    assert open(tmp2.name).read() == "Debug2\nInfo2\nWarning2\n"
+    assert open(tmp2).read() == "Debug2\nInfo2\nWarning2\n"
     assert stdout2.getvalue() == "Debug2\nInfo2\nWarning2\n"
